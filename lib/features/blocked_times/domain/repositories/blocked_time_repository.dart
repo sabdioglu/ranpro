1d10476e7fe8bfc594b24152a078f37f711d9adf
sabdioglu <sabdioglu@gmail.com>
2026-09-25 23:04:26 +0300

    Update blocked_time_repository.dart

--- lib/features/blocked_times/domain/repositories/blocked_time_repository.dart
+++ lib/features/blocked_times/domain/repositories/blocked_time_repository.dart
@@ -3,5 +3,6 @@ import '../entities/blocked_time_entity.dart';
 abstract class BlockedTimeRepository {
   Future<List<BlockedTimeEntity>> getBlockedTimes(String businessId, {String? staffId, DateTime? startDate, DateTime? endDate});
   Future<void> addBlockedTime(BlockedTimeEntity blockedTime);
+  Future<void> updateBlockedTime(BlockedTimeEntity blockedTime);
   Future<void> deleteBlockedTime(String blockedTimeId);
 }
