9a43c5f84617e095b48045402a247a416f656801
sabdioglu <sabdioglu@gmail.com>
2026-09-25 11:06:51 +0300

    Create lib/features/authentication/data/datasources/auth_remote_data_source.dart

---
+++ lib/features/authentication/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthRemoteDataSource {
+  Stream<User?> get authStateChanges;
+  User? get currentUser;
+  Future<UserCredential> signInWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+  Future<void> sendPasswordResetEmail(String email);
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthRemoteDataSourceImpl(this._firebaseAuth);
+
+  @override
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  @override
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  @override
+  Future<UserCredential> signInWithEmailAndPassword(String email, String password) async {
+    return await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  @override
+  Future<void> sendPasswordResetEmail(String email) async {
+    await _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+}
