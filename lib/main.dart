79cfdde654e37b471b787751b2a2ea564c16fb93
sabdioglu <sabdioglu@gmail.com>
2026-09-25 11:08:20 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 import 'core/utils/app_logger.dart';
+import 'firebase/firebase_initializer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
+  AppLogger.info('Firebase baslatiliyor...');
+  await FirebaseInitializer.initialize();
   AppLogger.info('RanPro Baslatiliyor');
   
   runApp(
