adbdd7a1914dc774c3ea1aad45a993dad70011c3
sabdioglu <sabdioglu@gmail.com>
2026-09-25 11:14:50 +0300

    Update auth_providers.dart

--- lib/features/authentication/presentation/providers/auth_providers.dart
+++ lib/features/authentication/presentation/providers/auth_providers.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../data/datasources/auth_remote_data_source.dart';
+import '../../data/datasources/user_remote_data_source.dart';
 import '../../data/repositories/auth_repository_impl.dart';
 import '../../domain/repositories/auth_repository.dart';
 import '../../domain/entities/app_user.dart';
@@ -9,14 +11,24 @@ final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
   return FirebaseAuth.instance;
 });
 
+final firebaseFirestoreProvider = Provider<FirebaseFirestore>((ref) {
+  return FirebaseFirestore.instance;
+});
+
 final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref) {
   final firebaseAuth = ref.watch(firebaseAuthProvider);
   return AuthRemoteDataSourceImpl(firebaseAuth);
 });
 
+final userRemoteDataSourceProvider = Provider<UserRemoteDataSource>((ref) {
+  final firestore = ref.watch(firebaseFirestoreProvider);
+  return UserRemoteDataSourceImpl(firestore);
+});
+
 final authRepositoryProvider = Provider<AuthRepository>((ref) {
-  final remoteDataSource = ref.watch(authRemoteDataSourceProvider);
-  return AuthRepositoryImpl(remoteDataSource);
+  final authDataSource = ref.watch(authRemoteDataSourceProvider);
+  final userDataSource = ref.watch(userRemoteDataSourceProvider);
+  return AuthRepositoryImpl(authDataSource, userDataSource);
 });
 
 final authStateProvider = StreamProvider<AppUser?>((ref) {
