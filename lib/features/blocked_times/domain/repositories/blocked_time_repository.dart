c020edea56a401b49f1226d187658e8ad901c855
sabdioglu <sabdioglu@gmail.com>
2026-09-25 22:53:49 +0300

    Create lib/features/blocked_times/domain/repositories/blocked_time_repository.dart

---
+++ lib/features/blocked_times/domain/repositories/blocked_time_repository.dart
@@ -0,0 +1,7 @@
+import '../entities/blocked_time_entity.dart';
+
+abstract class BlockedTimeRepository {
+  Future<List<BlockedTimeEntity>> getBlockedTimes(String businessId, {String? staffId, DateTime? startDate, DateTime? endDate});
+  Future<void> addBlockedTime(BlockedTimeEntity blockedTime);
+  Future<void> deleteBlockedTime(String blockedTimeId);
+}
