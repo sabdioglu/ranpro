4508736b486b4f3cfc3f92ea241f7ca2f1e5f4ff
sabdioglu <sabdioglu@gmail.com>
2026-09-25 11:11:53 +0300

    Create lib/features/authentication/data/datasources/user_remote_data_source.dart

---
+++ lib/features/authentication/data/datasources/user_remote_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+abstract class UserRemoteDataSource {
+  Future<Map<String, dynamic>?> getUserData(String uid);
+}
+
+class UserRemoteDataSourceImpl implements UserRemoteDataSource {
+  final FirebaseFirestore _firestore;
+
+  UserRemoteDataSourceImpl(this._firestore);
+
+  @override
+  Future<Map<String, dynamic>?> getUserData(String uid) async {
+    final doc = await _firestore.collection('users').doc(uid).get();
+    if (doc.exists) {
+      return doc.data();
+    }
+    return null;
+  }
+}
